e74ad9ff0de0a0304135b4d83c7b66407d45db6e
Soulai Vang <soulaivang@gmail.com>
2024-02-17 17:39:06 -0600

    Edited title screen and change scene script

--- Assets/Scripts/ChangeScene.cs
+++ Assets/Scripts/ChangeScene.cs
@@ -5,8 +5,9 @@ using UnityEngine.SceneManagement;
 
 public class ChangeScene : MonoBehaviour
 {
+    public string SceneName;
     public void PlayGameScene()
     {
-        SceneManager.LoadScene("MainGame");
+        SceneManager.LoadScene(SceneName);
     }
 }
