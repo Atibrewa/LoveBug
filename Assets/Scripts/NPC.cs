eb63cc05cce241531f63b784ae334ae81a1cc3d7
Kaliana Andriamananjara <kandriam@macalester.edu>
2024-02-18 13:06:35 -0600

    Update NPC.cs

--- Assets/Scripts/NPC.cs
+++ Assets/Scripts/NPC.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class NPC : MonoBehaviour
 {
-
+    public int symSum, score;
     public bool sweaty, fever, cough, welts, misty, heartbeat;
+    public float temp;
+    public int sicknessThreshold;
 
     // Start is called before the first frame update
     void Start()
@@ -18,9 +20,27 @@ public class NPC : MonoBehaviour
         cough = true;
         welts = true;
         misty = true;
-        heartbeat = true;
+        heartbeat = true;        
         
     }
+    
+    /// Generates char variables 
+    void generateNPC(int symSumInp)
+    {
+        symSum = symSumInp;
+        score = sicknessThreshold-symSum;
+    }
+
+    void setTemp()
+    {
+        float randFloat;
+        if fever {
+            temp = UnityEngine.Random.Range(99.5f, 106.0f);
+        } else {
+            temp = UnityEngine.Random.Range(97.5f, 99.4f);
+        }
+    }
+
 
     // Update is called once per frame
     void Update()
