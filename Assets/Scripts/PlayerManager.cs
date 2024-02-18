40b7e7dc8eb30109c080bd14b00cfcbe68645002
Atibrewa <atibrewa@macalester.edu>
2024-02-18 13:08:56 -0600

    started a player script

---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    public int day, dayScore, totalScore;
+    // needs to add time
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        day, dayScorescore, totalScore = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
