eec0377220856ddaa504d38f48fdd220b8ef7803
Atibrewa <atibrewa@macalester.edu>
2024-02-18 13:01:49 -0600

    added bg, changed layers

--- Assets/Scripts/ThermometerUse.cs
+++ Assets/Scripts/ThermometerUse.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class ThermometerUse : MonoBehaviour
 {
     public GameObject client;
-    bool fever;
     float temp;
 
     // Start is called before the first frame update
@@ -24,16 +23,17 @@ public class ThermometerUse : MonoBehaviour
 
     void CheckTemp()
     {
-        if !temp {
-            fever = client.GetComponent<NPC>().fever;
-            float randFloat;
-            if fever {
-                randFloat = UnityEngine.Random.Range(99.5f, 106.0f);
-            } else {
-                randFloat = UnityEngine.Random.Range(97.5f, 99.4f);
-            }
-            temp = randFloat;
-        }
+        temp = client.GetComponent<NPC>().temp;
+        // if !temp {
+        //     fever = client.GetComponent<NPC>().fever;
+        //     float randFloat;
+        //     if fever {
+        //         randFloat = UnityEngine.Random.Range(99.5f, 106.0f);
+        //     } else {
+        //         randFloat = UnityEngine.Random.Range(97.5f, 99.4f);
+        //     }
+        //     temp = randFloat;
+        // }
         
     }
 }
