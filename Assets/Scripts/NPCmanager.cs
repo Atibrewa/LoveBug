bb9f926a90cb2e7fd8aa0912d141942c70d13ea4
Kaliana Andriamananjara <kandriam@macalester.edu>
2024-02-18 14:36:38 -0600

    NPC Manager Bracket Fix

--- Assets/Scripts/NPCmanager.cs
+++ Assets/Scripts/NPCmanager.cs
@@ -19,36 +19,35 @@ public class NPCmanager : MonoBehaviour
     {
         string[][] dayList;
 
-        string[] p0 = ["Barry", "Newton", "18", "M", "St Paul"];
-        string[] p0s = [false, false, false, false, false, false];
+        string[] p0 = {"Barry", "Newton", "18", "M", "St Paul"};
+        string[] p0s = {false, false, false, false, false, false};
 
-        string[] p1 = ["Aahanaa", "Tibrewal", "20", "F", "St Paul"];
-        string[] p1s= [true, true, true, false, false, true];
+        string[] p1 = {"Aahanaa", "Tibrewal", "20", "F", "St Paul"};
+        string[] p1s= {true, true, true, false, false, true};
 
-        string[] p2 = ["Karen", "White", "48", "F", "Edina"];
-        string[] p2s = [true, false, true, false, false, false];
+        string[] p2 = {"Karen", "White", "48", "F", "Edina"};
+        string[] p2s = {true, false, true, false, false, false};
+        string[] p3 = {"Soulai", "Vang", "21", "M", "St Paul"};
+        string[] p3s = {true, false, false, false, false, true};
 
-        string[] p3 = ["Soulai", "Vang", "21", "M", "St Paul"];
-        string[] p3s = [true, false, false, false, false, true];
+        string[] p4 = {"Uni", "Vang", "20", "M", "St Paul"};
+        string[] p4s = {true, true, false, false, false, true};
 
-        string[] p4 = ["Uni", "Vang", "20", "M", "St Paul"];
-        string[] p4s = [true, true, false, false, false, true];
+        string[] p5 = {"Jerome", "Brown", "21" "M", "Edina"};
+        string[] p5s = {true, true, true, false, false, false};
 
-        string[] p5 = ["Jerome", "Brown", "21" "M", "Edina"];
-        string[] p5s = [true, true, true, false, false, false];
+        string[] p6 = {"Kaliana", "Andria", "20", "X", "St Paul"};
+        string[] p6s = {false, false, true, false, false, false};
 
-        string[] p6 = ["Kaliana", "Andria", "20", "X", "St Paul"];
-        string[] p6s = [false, false, true, false, false, false];
+        string[] p7 = {"Lucy", "Schroepfer", "44", "F", "St Paul"};
+        string[] p7s = {true, false, true, false, false, false};
 
-        string[] p7 = ["Lucy", "Schroepfer", "44", "F", "St Paul"];
-        string[] p7s = [true, false, true, false, false, false];
+        string[] p8 = {"Gavin", "OBrien", "24", "M", "Edina"};
+        string[] p8s = {true, false, false, false, false, false};
 
-        string[] p8 = ["Gavin", "OBrien", "24", "M", "Edina"];
-        string[] p8s = [true, false, false, false, false, false];
+        string[] p9 = {"Lulu", "Lemonholm", "7", "F", "St Paul"};
+        string[] p9s = {false, true, true, false, false, false};
 
-        string[] p9 = ["Lulu", "Lemonholm", "7", "F", "St Paul"];
-        string[] p9s = [false, true, true, false, false, false];
-        
         return dayList;
     }
 
